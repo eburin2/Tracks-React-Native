ef94fc4d8e95e4f93da4b863fffea8fb6942e488
Elliot Burin <eburin2@gmail.com>
2020-01-19 15:37:11 -0800

    add mapview to map component

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import { Text, StyleSheet } from 'react-native';
+import MapView, { Polyline } from 'react-native-maps';
 
 const Map = () => {
+  let points = [];
+  for (let i = 0; i < 20; i++){
+    if (i % 2 === 0) {
+      points.push({
+        latitude: 45.5415 + i * 0.001,
+        longitude: -122.6046 + i * 0.001
+      });
+    } else {
+      points.push({
+        latitude: 45.5415 - i * 0.002,
+        longitude: -122.6046 + i * 0.001
+      });
+    }
+  }
   return (
-    <Text>I am a map</Text>
+    <MapView
+    style={styles.map}
+    initialRegion={{
+      latitude: 45.5415,
+      longitude: -122.6046,
+      latitudeDelta: 0.01,
+      longitudeDelta: 0.01
+    }}
+  >
+    <Polyline coordinates={points}/>
+  </MapView>
   )
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  map: {
+    height: 300
+  }
+});
 
 export default Map;
